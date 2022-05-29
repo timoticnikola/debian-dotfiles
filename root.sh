6e89e05424fc1500c7b7f4f8557ebcd4cacb6ad4
Nikola Timotic <timoticnikola.01@gmail.com>
2022-05-29 04:00:12 -0700

    first commit

--- root.sh
+++ root.sh
@@ -14,7 +14,7 @@ curl -s https://brave-browser-apt-release.s3.brave.com/brave-core.asc | sudo apt
 echo "deb [arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
 sudo apt update && sudo apt install brave-browser
 
-
+bash obsidian.sh
 
 # sddm - lock screen
 # psmisc - killall command
