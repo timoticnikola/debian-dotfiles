1be6fd862de1df302c280c414c85ce7e7e2fefc6
Nikola Timotic <timoticnikola.01@gmail.com>
2022-05-30 11:31:24 +0200

    Small fix

--- root.sh
+++ root.sh
@@ -4,15 +4,19 @@
 cp /etc/apt/sources.list /etc/apt/sources.list.bak
 cp sources.list /etc/apt/sources.list 
 
-sudo apt install vim ssdm i3 polybar psmisc apt-transport-https -y
+sudo apt install vim sddm i3 polybar psmisc apt-transport-https -y
 
-sudo apt install apt-transport-https
 
 # brave browser
-sudo apt install curl bnupg bnupg2 gnupg1 -y
-curl -s https://brave-browser-apt-release.s3.brave.com/brave-core.asc | sudo apt-key --keyring /etc/apt/trusted.gpg.d/brave-browser-release.gpg add -
-echo "deb [arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
-sudo apt update && sudo apt install brave-browser
+sudo apt install apt-transport-https curl -y
+
+sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+
+echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+
+sudo apt update -y
+
+sudo apt install brave-browser -y
 
 bash obsidian.sh
 
